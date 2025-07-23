bff8b11e5de6b48c6c5fc7e9770d25f51d3d5df8
armazade <armantje2001@gmail.com>
2025-07-23 23:01:37 +0200

    fix: rename ProductUpdateForm component and integrate it into the Edit page

--- resources/js/Pages/Admin/Product/Edit.vue
+++ resources/js/Pages/Admin/Product/Edit.vue
@@ -11,7 +11,7 @@
     <div class="mx-auto max-w-7xl space-y-6 sm:px-6 lg:px-8">
       <div class="bg-white p-4 shadow sm:rounded-lg sm:p-8">
         <h1 class="text-2xl font-bold mb-4">Edit: {{ product.name }}</h1>
-        <UpdateProductForm :product="product" class="max-w-xl" />
+        <ProductUpdateForm :product="product" class="max-w-xl" />
       </div>
     </div>
   </div>
@@ -19,7 +19,7 @@
 
 <script setup>
 import { Head } from '@inertiajs/vue3'
-import UpdateProductForm from '@/Components/UpdateProductForm.vue'
+import ProductUpdateForm from '@/Components/ProductUpdateForm.vue'
 
 const props = defineProps({
   product: Object,
