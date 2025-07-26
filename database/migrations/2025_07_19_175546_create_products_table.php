c2732892ef2d4224f778a542df59c912a36b9712
armazade <armantje2001@gmail.com>
2025-07-26 14:40:59 +0200

    fix: remove unnecessary indent

--- database/migrations/2025_07_19_175546_create_products_table.php
+++ database/migrations/2025_07_19_175546_create_products_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         $table->timestamps();
         $table->string('name');
         $table->text('description')->nullable();
-            $table->float('price');
+        $table->float('price');
         });
     }
 
